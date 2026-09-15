c10dad4cd95b57eab8e97fafe3b6c4bc887c050f
mavor <ma_voroshilov@mail.ru>
2026-09-15 23:32:35 +0300

    Sentences

---
+++ sentence/sentence_sequence.py
@@ -0,0 +1,16 @@
+from utils.exception import InternalError
+from sentence.sentence import Sentence
+
+
+class SentenceSequence:
+    def __init__(self):
+        # For now, there is no need to process multiple sentences
+        self.sentence = None
+
+    def add_sentence(self, sentence: Sentence) -> None:
+        self.sentence = sentence
+
+    def execute(self) -> None:
+        if self.sentence is None:
+            raise InternalError('Internal error')
+        self.sentence.execute()
