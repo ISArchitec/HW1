4265ffbf19e2225825be34d976b9d606c4b8bfb9
verbinna22 <mylogininya@yandex.ru>
2026-09-26 19:55:17 +0300

    fix: description

--- sentence/sentence_sequence.py
+++ sentence/sentence_sequence.py
@@ -13,7 +13,7 @@ class SentenceSequence:
         self.session = session
 
     def add_sentence(self, sentence: Sentence) -> None:
-        "adds sentence in sequence"
+        """Adds sentence in sequence"""
         self.__sentence.append(sentence)
 
     def __iter__(self):
